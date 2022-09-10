8d33c5cd435559f6dbd11e258d7dd4def225b389
Verina Ong Dinata <verina.od1997@gmail.com>
2022-09-10 11:09:25 +0800

    run unit and system tests on GHA

---
+++ test/system/products_test.rb
@@ -0,0 +1,23 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit products_url
+  #
+  #   assert_selector "h1", text: "Product"
+  # end
+  test "lets a signed in user create a new product" do
+    login_as users(:george)
+    visit "/products/new"
+    # save_screenshot
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+    # save_screenshot
+
+    click_on "Create Product"
+    # save_screenshot
+
+    assert_text "Change your life: Learn to code"
+  end
+end
